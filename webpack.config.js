adc676fca0b78f21084a5ee19f8ac41c1fcc7660
mobidiic <sigus128@gmail.com>
2018-08-29 12:33:37 +0900

    feat: basic type and stylesheets
    and change the setup by babel version up

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
                 exclude: /(node_modules)/,
                 loader: 'babel-loader',
                 query: {
-                    presets: ['env', 'stage-0', 'react']
+                    presets: ['@babel/env', '@babel/react']
                 }
             },
             {
