33c9836407ecc1c10a612a75f57961c88744c4b6
mobidiic <sigus128@gmail.com>
2018-08-29 20:25:53 +0900

    feat: set loading spinkit
     but api doesn't connected :(

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,59 @@ import ViewerTemplate from './ui/ViewerTemplate'
 import SpaceNavigator from './ui/SpaceNavigator'
 import Viewer from './ui/Viewer'
 import * as api from '../lib/api'
+import moment from 'moment'
 
 class App extends Component{
 
+  state ={
+    loading: false,
+    maxDate: null,
+    date: null,
+    url: null,
+    mediaType: null
+  }
+
   getAPOD = async(date) => {
+    if(this.state.loading){
+      return
+    }
+    this.setState({
+      loading: true
+    })
    try{
      const response = await api.getAPOD(date)
-     console.log(response)
+     const { date: retrievedDate, url, media_type: mediaType} =response.data
+     if(!this.state.maxDate){
+       this.setState({
+         maxDate: retrievedDate
+       })
+
+      this.setState({
+        date: retrievedDate,
+        mediaType,
+        url
+      })
+     }
    }catch(e){
      console.log(e)
+     this.setState({
+       loading: false
+     })
    }
+ }
+
+ handlePrev = () => {
+   const { date } = this.state
+   const prevDate = moment(date).subtract(1, 'days').format('YYYY-MM-DD')
+   console.log(prevDate)
+   this.getAPOD(prevDate)
+ }
+
+ handleNext = () => {
+   const { date } = this.state
+   const nextDate = moment(date).add(1, 'days').format('YYYY-MM-DD')
+   console.log(nextDate)
+   this.getAPOD(nextDate)
  }
 
   componentDidMount(){
@@ -20,12 +63,15 @@ class App extends Component{
   }
 
   render(){
+    const { url, mediaType, loading } =this.state
+    const { handlePrev, handleNext } = this
+
     return (
       <ViewerTemplate
-        spaceNavigator={<SpaceNavigator/>}
-        viewer={<Viewer
-                    url="https://www.youtube.com/embed/uj3Lq7Gu94Y?rel=0"
-                    mediaType="video" />}
+        spaceNavigator={<SpaceNavigator onPrev={handlePrev} onNext={handleNext}/>}
+        viewer={<Viewer url={url}
+                        mediaType={mediaType}
+                        loading={loading}/>}
         />
     )
   }
--- src/components/ui/Viewer.js
+++ src/components/ui/Viewer.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
 import styles from '../../../stylesheets/Viewer.scss'
 import classNames from 'classnames/bind'
+import { ChasingDots } from 'better-react-spinkit'
 
 const cx = classNames.bind(styles)
 
 const Viewer = ({url, mediaType, loading}) => {
+  if(loading){
+    return(
+      <div className={cx('viewer')}>
+        <ChasingDots color="white" size={40} />
+      </div>
+    )
+  }
   if(!url) return null
   else return (
     <div className={cx('viewer')}>
